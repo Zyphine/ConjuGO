1852cfe33a6da0c1fe919e92f3990a33f5f3685f
Zyphine <tarrou.theo@gmail.com>
2023-12-12 15:12:05 +0100

    test

--- lib/about.dart
+++ lib/about.dart
@@ -3,7 +3,7 @@ import 'package:conjugo/DrawerMenu.dart';
 
 //Page à propos
 class About extends StatelessWidget {
-  const About({super.key}); //Git GF
+  const About({super.key}); //Git GF test
 
   @override
   Widget build(BuildContext context) {
