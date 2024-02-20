0b95576b0b2fbabd1e537c3ba2a2a21cee2f36fb
Rayyze <chppleo@gmail.com>
2024-02-20 17:10:51 +0100

    refresh la page lors de l'inscription

--- lib/activityDescription.dart
+++ lib/activityDescription.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:conjugo/listActivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -152,6 +153,7 @@ class DescriptionPage extends StatelessWidget {
   }
 
   Future<void> registerParticipant(BuildContext context) async {
+    print("inscription en cours");
     final publication = FirebaseFirestore.instance.collection("ACTIVITYDATA").doc(documentId);
     publication.get().then(
       (DocumentSnapshot doc) {
@@ -167,7 +169,10 @@ class DescriptionPage extends StatelessWidget {
               content: const Text("Vous êtes pré-inscrit avec succès \npensez à vous inscrire auprès de l'organisateur"),
               actions: <Widget>[
                 TextButton(
-                  onPressed: () => Navigator.pop(context, 'OK'),
+                  onPressed: () => {Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ListViewHomeLayout()),
+                    )},
                   child: const Text('OK'),
                 ),
               ],
