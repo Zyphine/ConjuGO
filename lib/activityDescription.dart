3bf8fa472dd0edbaba4224f2d0b10ea3ede38348
Rayyze <chppleo@gmail.com>
2024-02-21 14:14:27 +0100

    nouveau design activityDescription

--- lib/activityDescription.dart
+++ lib/activityDescription.dart
@@ -26,137 +26,95 @@ class DescriptionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          //Le titre de la page est le name de l'activité sur laquelle nous avons appuyée
-          title: Text(name),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: [
-                //Image
-                Container(
-                  margin: const EdgeInsets.all(50),
-                  width: 100,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      //Image générique à remplacer : soit le logo de l'association, soit une photo de l'activité
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://assistance-sociale.fr/wp-content/uploads/2021/12/ccas-douai"),
-                        fit: BoxFit.cover,
-                      )),
-                ),
-                Container(
-                    //Places restantes
-                    padding: const EdgeInsets.all(16.0),
-                    margin: const EdgeInsets.only(bottom: 30),
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              child: Stack(
+                children: [
+                  Container(
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blue, width: 2)),
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Places\nrestantes',
-                          style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              decoration: TextDecoration.underline),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          numberOfRemainingEntries.toString(),
-                          style: const TextStyle(color: Colors.blue, fontSize: 20),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ))
-              ],
-            ),
-            Container(
-              //Date
-              width: 300,
-              height: 100,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Quand :',
-                      style: TextStyle(color: Colors.blue, fontSize: 30),
-                      textAlign: TextAlign.left),
-                  Text(date,
-                      style: const TextStyle(color: Colors.black, fontSize: 20),
-                      textAlign: TextAlign.left),
-                ],
-              ),
-            ),
-            Container(
-              //place
-              width: 300,
-              height: 100,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Où :',
-                      style: TextStyle(color: Colors.blue, fontSize: 30),
-                      textAlign: TextAlign.left),
-                  Text(place,
-                      style: const TextStyle(color: Colors.black, fontSize: 20),
-                      textAlign: TextAlign.left),
-                ],
-              ),
-            ),
-            Container(
-              //Description
-              width: 300,
-              height: 100,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Description :',
-                      style: TextStyle(color: Colors.blue, fontSize: 30),
-                      textAlign: TextAlign.left),
-                  Text(
-                    description,
-                    style: const TextStyle(color: Colors.black, fontSize: 20),
-                    textAlign: TextAlign.left,
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Description :',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            description,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: IconButton(
+                      icon: const Icon(Icons.favorite),
+                      color: Colors.red,
+                      onPressed: () {
+                        // Implement add to favorites logic here
+                      },
+                    ),
                   ),
                 ],
               ),
             ),
-            Container(
-                //Bouton inscription
-                margin: const EdgeInsets.only(left: 100.0, top: 20.0),
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: TextButton(
-                    style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.black),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color.fromARGB(255, 222, 205, 51)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: const BorderSide(color: Colors.black)))),
-                    onPressed: () => registerParticipant(context),
-                    //Pour l'instant ne fait rien, à implémenter. Soit avec systèmes favoris, soit inscription avec décompte des places
-                    child: Text("Je m'inscris".toUpperCase(),
-                        style: const TextStyle(fontSize: 14))
-                    ),
-                ))
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Date :',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(date),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Places restantes :',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(numberOfRemainingEntries.toString()),
+                ],
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () => registerParticipant(context),
+              child: const Text('Pré-inscription'),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 
   Future<void> registerParticipant(BuildContext context) async {
-    print("inscription en cours");
     final publication = FirebaseFirestore.instance.collection("ACTIVITYDATA").doc(documentId);
     publication.get().then(
       (DocumentSnapshot doc) {
